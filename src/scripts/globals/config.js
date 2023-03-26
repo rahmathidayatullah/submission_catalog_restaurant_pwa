ebb142e7788072a6d770ae99c636f16d2aa6f89d
rahmathidayatullah <rahmathidayatullah996@gmail.com>
2023-03-26 16:22:49 +0700

    cathing data offline

--- src/scripts/globals/config.js
+++ src/scripts/globals/config.js
@@ -5,6 +5,8 @@ const CONFIG = {
   BASE_IMAGE_URL_MEDIUM: 'https://restaurant-api.dicoding.dev/images/medium',
   BASE_IMAGE_URL_LARGE: 'https://restaurant-api.dicoding.dev/images/large',
   // DEFAULT_LANGUAGE: 'en-us',
+  // CACHE_NAME: 'RestaurantCatalogue-V1',
+  CACHE_NAME: new Date().toISOString(),
 };
 
 export default CONFIG;
