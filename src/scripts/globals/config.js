d40534e8b2c7643c9162676cfd42e393dfe5dbb4
rahmathidayatullah <rahmathidayatullah996@gmail.com>
2023-04-22 23:03:51 +0700

    fixing issue after last submit pwa

--- src/scripts/globals/config.js
+++ src/scripts/globals/config.js
@@ -4,14 +4,18 @@ const CONFIG = {
   BASE_IMAGE_URL_SMALL: 'https://restaurant-api.dicoding.dev/images/small',
   BASE_IMAGE_URL_MEDIUM: 'https://restaurant-api.dicoding.dev/images/medium',
   BASE_IMAGE_URL_LARGE: 'https://restaurant-api.dicoding.dev/images/large',
-  CACHE_NAME: new Date().toISOString(),
+  // CACHE_NAME: new Date().toISOString(),
+  CACHE_NAME: 'cache-restaurant',
   DATABASE_NAME: 'restaurant-catalogue-database',
   DATABASE_VERSION: 1,
   OBJECT_STORE_NAME: 'restaurants',
   WEB_SOCKET_SERVER: 'wss://movies-feed.dicoding.dev',
-  PUSH_MSG_VAPID_PUBLIC_KEY: 'BN7-r0Svv7CsTi18-OPYtJLVW0bfuZ1x1UtrygczKjennA_qs7OWmgOewcuYSYF3Gc_mPbqsDh2YoGCDPL0RxDQ',
-  PUSH_MSG_SUBSCRIBE_URL: 'https://dicoding-movie-push-notif.netlify.app/.netlify/functions/subscribe',
-  PUSH_MSG_UNSUBSCRIBE_URL: 'https://dicoding-movie-push-notif.netlify.app/.netlify/functions/unsubscribe',
+  PUSH_MSG_VAPID_PUBLIC_KEY:
+    'BN7-r0Svv7CsTi18-OPYtJLVW0bfuZ1x1UtrygczKjennA_qs7OWmgOewcuYSYF3Gc_mPbqsDh2YoGCDPL0RxDQ',
+  PUSH_MSG_SUBSCRIBE_URL:
+    'https://dicoding-movie-push-notif.netlify.app/.netlify/functions/subscribe',
+  PUSH_MSG_UNSUBSCRIBE_URL:
+    'https://dicoding-movie-push-notif.netlify.app/.netlify/functions/unsubscribe',
 };
 
 export default CONFIG;
